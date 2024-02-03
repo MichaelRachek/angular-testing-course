f4cce42d0ba82c43267697b1423c50a1c8e9992a
mrachek <mrachek@asocscloud.com>
2024-02-03 20:06:27 +0200

    added tests for home component

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,20 +1,14 @@
-import { ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, TestBed, waitForAsync } from '@angular/core/testing';
 import {CoursesModule} from '../courses.module';
 import {DebugElement} from '@angular/core';
 
 import {HomeComponent} from './home.component';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {CoursesService} from '../services/courses.service';
-import {HttpClient} from '@angular/common/http';
-import {COURSES} from '../../../../server/db-data';
 import {setupCourses} from '../common/setup-test-data';
 import {By} from '@angular/platform-browser';
 import {of} from 'rxjs';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {click} from '../common/test-utils';
-
-
-
+import { CoursesService } from '../services/courses.service';
 
 describe('HomeComponent', () => {
 
@@ -32,9 +26,7 @@ describe('HomeComponent', () => {
 
 
   beforeEach(waitForAsync(() => {
-
       const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses'])
-
       TestBed.configureTestingModule({
           imports: [
               CoursesModule,
@@ -54,103 +46,56 @@ describe('HomeComponent', () => {
   }));
 
   it("should create the component", () => {
-
     expect(component).toBeTruthy();
-
   });
 
-
-  it("should display only beginner courses", () => {
-
+//ngIf
+  it('should display only beginner courses', () => {
       coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
-
       fixture.detectChanges();
-
-      const tabs = el.queryAll(By.css(".mdc-tab"));
-
-      expect(tabs.length).toBe(1, "Unexpected number of tabs found");
+      const tabs = el.queryAll(By.css('.mdc-tab'));
+      expect(tabs.length).withContext('Unexpected number of tabs found').toBe(1);
 
   });
 
-
-  it("should display only advanced courses", () => {
-
+  it('should display only advanced courses', () => {
       coursesService.findAllCourses.and.returnValue(of(advancedCourses));
-
       fixture.detectChanges();
-
-      const tabs = el.queryAll(By.css(".mdc-tab"));
-
-      expect(tabs.length).toBe(1, "Unexpected number of tabs found");
-
+      const tabs = el.queryAll(By.css('.mdc-tab'));
+      expect(tabs.length).withContext('Unexpected number of tabs found').toBe(1);
   });
 
-
-  it("should display both tabs", () => {
-
+  it('should display both tabs', () => {
       coursesService.findAllCourses.and.returnValue(of(setupCourses()));
-
       fixture.detectChanges();
-
-      const tabs = el.queryAll(By.css(".mdc-tab"));
-
-      expect(tabs.length).toBe(2, "Expected to find 2 tabs");
-
+      const tabs = el.queryAll(By.css('.mdc-tab'));
+      expect(tabs.length).withContext('Expected to find 2 tabs').toBe(2);
   });
-
 
   it("should display advanced courses when tab clicked - fakeAsync", fakeAsync(() => {
-
       coursesService.findAllCourses.and.returnValue(of(setupCourses()));
-
       fixture.detectChanges();
-
       const tabs = el.queryAll(By.css(".mdc-tab"));
-
       click(tabs[1]);
-
       fixture.detectChanges();
-
       flush();
-
       const cardTitles = el.queryAll(By.css('.mat-mdc-tab-body-active .mat-mdc-card-title'));
-
-      console.log(cardTitles);
-
-      expect(cardTitles.length).toBeGreaterThan(0,"Could not find card titles");
-
-      expect(cardTitles[0].nativeElement.textContent).toContain("Angular Security Course");
-
+      expect(cardTitles.length).withContext('Could not find card titles').toBe(3);
+      expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
   }));
 
-
-    it("should display advanced courses when tab clicked - async", waitForAsync(() => {
-
+    it('should display advanced courses when tab clicked - async', waitForAsync(() => {
         coursesService.findAllCourses.and.returnValue(of(setupCourses()));
-
         fixture.detectChanges();
-
         const tabs = el.queryAll(By.css(".mdc-tab"));
-
         click(tabs[1]);
-
         fixture.detectChanges();
-
         fixture.whenStable().then(() => {
-
-            console.log("called whenStable() ");
-
             const cardTitles = el.queryAll(By.css('.mat-mdc-tab-body-active .mat-mdc-card-title'));
-
-            expect(cardTitles.length).toBeGreaterThan(0,"Could not find card titles");
-
-            expect(cardTitles[0].nativeElement.textContent).toContain("Angular Security Course");
-
+            expect(cardTitles.length).withContext('Could not find card titles').toBe(3);
+            expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
         });
-
     }));
-
-
 });
 
 
--- src/app/courses/services/courses.services.spec.ts
+++ src/app/courses/services/courses.services.spec.ts
@@ -1,156 +1,101 @@
-import {CoursesService} from './courses.service';
-import {TestBed} from '@angular/core/testing';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {COURSES, findLessonsForCourse} from '../../../../server/db-data';
-import {Course} from '../model/course';
-import {HttpErrorResponse} from '@angular/common/http';
-
+import { CoursesService } from './courses.service';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { COURSES, findLessonsForCourse } from '../../../../server/db-data';
 
 describe('CoursesService', () => {
-
-    let coursesService: CoursesService,
-        httpTestingController: HttpTestingController;
-
-    beforeEach(() => {
-
-        TestBed.configureTestingModule({
-            imports: [
-                HttpClientTestingModule
-            ],
-            providers: [
-                CoursesService
-            ]
-        });
-
-        coursesService = TestBed.inject(CoursesService),
-        httpTestingController = TestBed.inject(HttpTestingController);
-
+  let service: CoursesService;
+  let httpTestingController: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CoursesService]
     });
 
-    it('should retrieve all courses', () => {
+    service = TestBed.inject(CoursesService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
 
-        coursesService.findAllCourses()
-            .subscribe(courses => {
-
-                expect(courses).toBeTruthy('No courses returned');
-
-                expect(courses.length).toBe(12,
-                    "incorrect number of courses");
-
-                const course = courses.find(course => course.id == 12);
-
-                expect(course.titles.description).toBe(
-                    "Angular Testing Course");
-
-            });
-
-        const req = httpTestingController.expectOne('/api/courses');
-
-        expect(req.request.method).toEqual("GET");
-
-        req.flush({payload: Object.values(COURSES)});
-
+  it('should retrieve all courses', () => {
+    service.findAllCourses().subscribe(resp => {
+      expect(resp).withContext('No Courses returned');
+      expect(resp.length).withContext('incorrect number of courses').toEqual(12);
+      const course = resp.find(item => item.id === 12);
+      expect(course.titles.description).toBe('Angular Testing Course');
     });
 
-    it('should find a course by id', () => {
+    const req = httpTestingController.expectOne('/api/courses');
+    expect(req.request.method).toEqual('GET');
+    req.flush({payload: Object.values(COURSES)});
+    httpTestingController.verify();
+  });
 
-        coursesService.findCourseById(12)
-            .subscribe(course => {
+  it('should find course by id', () => {
+    const courseId = 10;
+    service.findCourseById(courseId).subscribe(resp => {
+      expect(resp).withContext('No course returned');
+      expect(resp).toBeTruthy();
+      expect(resp.id).toBe(10);
+      expect(resp.titles.description).toBe('Rxjs and Reactive Patterns Angular Architecture Course');
+    });
+    const req = httpTestingController.expectOne(`/api/courses/${courseId}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(COURSES[courseId]);
+    httpTestingController.verify();
+  });
 
-                expect(course).toBeTruthy();
-                expect(course.id).toBe(12);
+  it('should save course', () => {
+    const courseId = 5;
+    const changes = {titles: {description: 'New Description'}};
+    service.saveCourse(courseId, changes).subscribe(resp => {
+      expect(resp).withContext('No course returned');
+      expect(resp).toBeTruthy();
+      expect(resp.id).toBe(courseId);
+      expect(resp.titles.description).toBe('New Description');
+    });
+    const req = httpTestingController.expectOne(`/api/courses/${courseId}`);
+    expect(req.request.method).toEqual('PUT');
+    expect(req.request.body).toEqual(changes);
+    req.flush({...COURSES[courseId], ...changes});
+    httpTestingController.verify();
+  });
 
-            });
+  it('should give an error if save course fails', () => {
+    const changes = {titles: {description: 'Testing Course'}};
+    service.saveCourse(12, changes)
+      .subscribe(
+        () => fail('the save course operation should have failed'),
+        error => {
+          expect(error.status).toBe(500);
+        }
+      );
+    const req = httpTestingController.expectOne('/api/courses/12');
+    expect(req.request.method).toEqual('PUT');
+    req.flush('Save course failed', {
+      status: 500,
+      statusText: 'Internal Server Error'
+    });
+    httpTestingController.verify();
+  });
 
-        const req = httpTestingController.expectOne('/api/courses/12');
-
-        expect(req.request.method).toEqual("GET");
-
-        req.flush(COURSES[12]);
-
+  it('should find a list of lessons', () => {
+    service.findLessons(12).subscribe(resp => {
+      expect(resp).toBeTruthy();
+      expect(resp.length).toBeGreaterThan(0);
+      expect(resp.length).toBe(3);
     });
 
-    it('should save the course data', () => {
+    const req = httpTestingController.expectOne(req =>
+      req.url == '/api/lessons');
 
-        const changes :Partial<Course> =
-            {titles:{description: 'Testing Course'}};
-
-        coursesService.saveCourse(12, changes)
-            .subscribe(course => {
-
-                expect(course.id).toBe(12);
-
-            });
-
-        const req = httpTestingController.expectOne('/api/courses/12');
-
-        expect(req.request.method).toEqual("PUT");
-
-        expect(req.request.body.titles.description)
-            .toEqual(changes.titles.description);
-
-        req.flush({
-            ...COURSES[12],
-            ...changes
-        })
-
-    });
-
-    it('should give an error if save course fails', () => {
-
-        const changes :Partial<Course> =
-            {titles:{description: 'Testing Course'}};
-
-        coursesService.saveCourse(12, changes)
-            .subscribe(
-                () => fail("the save course operation should have failed"),
-
-                (error: HttpErrorResponse) => {
-                    expect(error.status).toBe(500);
-                }
-            );
-
-        const req = httpTestingController.expectOne('/api/courses/12');
-
-        expect(req.request.method).toEqual("PUT");
-
-        req.flush('Save course failed', {status:500,
-            statusText:'Internal Server Error'});
-    });
-
-    it('should find a list of lessons', () => {
-
-        coursesService.findLessons(12)
-            .subscribe(lessons => {
-
-                expect(lessons).toBeTruthy();
-
-                expect(lessons.length).toBe(3);
-
-            });
-
-        const req = httpTestingController.expectOne(
-            req => req.url == '/api/lessons');
-
-        expect(req.request.method).toEqual("GET");
-        expect(req.request.params.get("courseId")).toEqual("12");
-        expect(req.request.params.get("filter")).toEqual("");
-        expect(req.request.params.get("sortOrder")).toEqual("asc");
-        expect(req.request.params.get("pageNumber")).toEqual("0");
-        expect(req.request.params.get("pageSize")).toEqual("3");
-
-        req.flush({
-            payload: findLessonsForCourse(12).slice(0,3)
-        });
-
-
-    });
-
-    afterEach(() => {
-
-        httpTestingController.verify();
-    });
-
+    expect(req.request.method).toEqual('GET')
+    expect(req.request.params.get('courseId')).toEqual('12');
+    expect(req.request.params.get('filter')).toEqual('');
+    expect(req.request.params.get('sortOrder')).toEqual('asc');
+    expect(req.request.params.get('pageNumber')).toEqual('0');
+    expect(req.request.params.get('pageSize')).toEqual('3');
+    req.flush({payload: findLessonsForCourse(12).slice(0, 3)});
+  });
 });
 
 
